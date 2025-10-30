ea63f17f35279775e8c68fcf5ce8ae4f18443cba
eufolim <>
2025-10-30 20:17:04 -0300

    foi!

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,50 @@
 console.log("Hello, World! 001");
+
+class Company {
+    showDetails() {
+        throw new Error("Abstrato") 
+    }
+}
+
+class Task extends Company {
+    constructor(nome) {
+        super();
+        this.nome = nome;
+    }
+
+    showDetails(indent = "") {
+        console.log(`${indent}Tarefa: ${this.nome}`);
+    }
+}
+
+class Project extends Company {
+    constructor(nome) {
+        super();
+        this.nome = nome;
+        this.tasks = [];
+    }
+
+    add(task) {
+        this.tasks.push(task);
+    }
+
+    showDetails(indent = "") {
+        console.log(`${indent}Projeto: ${this.nome}`);
+        this.tasks.forEach((tasks) => tasks.showDetails(indent + "    "));
+    }
+}
+
+const p1 = new Project("Projeto Principal");
+const p2 = new Project("Dependencia");
+
+const t1 = new Task("desenvolver");
+const t2 = new Task("resolver compatbilidade");
+const t3 = new Task("installar");
+
+p2.add(t3);
+p2.add(t2);
+
+p1.add(p2);
+p1.add(t1);
+
+p1.showDetails()
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,33 @@
 console.log("Hello, World! 002");
+
+class TV {
+    constructor() {
+        this.power = false;
+    }
+    turnOn() {
+        this.power = true
+        console.log("TV ligada.");
+    }
+    turnOff() {
+        this.power = false
+        console.log("TV desligada.");
+    }
+}
+
+class Controler {
+    buttonPress(tv) {
+        if (tv.power == true) {
+            tv.turnOff();
+        } else {
+            tv.turnOn();
+        }
+    }
+}
+
+const televisao = new TV;
+const controle = new Controler;
+
+controle.buttonPress(televisao)
+controle.buttonPress(televisao)
+
+
