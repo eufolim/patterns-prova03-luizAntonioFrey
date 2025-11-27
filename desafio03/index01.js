5de54470d0d7d26ddeb60288535be6e31f8b49c6
eufolim <luiz.afc@outlook.com>
2025-11-27 20:04:56 -0300

    index1

--- desafio03/index01.js
+++ desafio03/index01.js
@@ -1,1 +1,36 @@
 console.log("Hello, World! 001");
+console.log("Substituição de Liskov");
+
+class estudante {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    estudar() {
+        console.log(this.nome + " está estudando")
+    }
+}
+
+class estudanteFundamental extends estudante{
+    recuperacao() {
+        console.log(this.nome + " ficou de recuperação")
+    }
+}
+
+class estudanteGraduacao extends estudante {
+    N1() {
+        console.log(this.nome + " ficou na n-1")
+    }
+}
+
+const estudante1 = new estudante("aluno1")
+const estudante2 = new estudanteFundamental("aluno2")
+const estudante3 = new estudanteGraduacao("aluno3")
+
+estudante1.estudar();
+estudante2.estudar();
+estudante3.estudar();
+
+estudante2.recuperacao();
+
+estudante3.N1();
